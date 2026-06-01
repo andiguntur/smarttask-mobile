2dfaaf64f35aff6d122f533c53df2ed03f8c9826
andiguntur <andigunturmario@gmail.com>
2026-06-01 18:23:18 +0700

    Add api dan axios

--- src/screens/auth/LoginScreen.tsx
+++ src/screens/auth/LoginScreen.tsx
@@ -8,6 +8,10 @@ import {
 } from 'react-native';
 
 export default function LoginScreen({ navigation }: any) {
+  const handleLogin = async () => {
+    const res = await login('test@mail.com', '123456');
+    console.log(res);
+  };
   return (
     <View style={styles.container}>
       <Text style={styles.title}>
@@ -25,10 +29,8 @@ export default function LoginScreen({ navigation }: any) {
         style={styles.input}
       />
 
-      <TouchableOpacity style={styles.button}>
-        <Text style={styles.buttonText}>
-          Login
-        </Text>
+      <TouchableOpacity style={styles.button} onPress={handleLogin}>
+        <Text style={styles.buttonText}>Login</Text>
       </TouchableOpacity>
 
       <TouchableOpacity onPress={() => navigation.navigate('Register')} >
@@ -36,6 +38,8 @@ export default function LoginScreen({ navigation }: any) {
           Go to Register
         </Text>
       </TouchableOpacity>
+
+      
 
     </View>
   );
@@ -64,7 +68,7 @@ const styles = StyleSheet.create({
   },
 
   button: {
-    backgroundColor: '#000',
+    backgroundColor: '#000000',
     padding: 15,
     borderRadius: 10,
     alignItems: 'center',
